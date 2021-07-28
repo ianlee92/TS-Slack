0717cb5e7eaaa768bd71f970a5d17c1008c540e0
ianlee92 <ethian9227@gmail.com>
2021-07-28 23:02:47 +0900

    DM 페이지 만들기

--- front/pages/DirectMessage/index.tsx
+++ front/pages/DirectMessage/index.tsx
@@ -1,7 +1,28 @@
+import ChatBox from '@components/ChatBox';
+import ChatList from '@components/ChatList';
+import { Container, Header } from '@pages/Channel/styles';
+import fetcher from '@utils/fetcher';
 import React from 'react';
+import gravatar from 'gravatar';
+import { useParams } from 'react-router';
+import useSWR from 'swr';
 
 const DirectMessage = () => {
-  return <div>DM</div>;
+  const { workspace, id } = useParams<{ workspace: string; id: string }>();
+  const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
+  const { data: myData } = useSWR('/api/users', fetcher);
+  if (!userData || !myData) {
+    return null;
+  }
+  return (
+    <Container>
+      <Header>
+        <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
+      </Header>
+      <ChatList />
+      <ChatBox chat="" />
+    </Container>
+  );
 };
 
 export default DirectMessage;
