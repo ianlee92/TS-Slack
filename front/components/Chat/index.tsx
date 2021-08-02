f514cc194758f205cc5956fc72a656aba425930b
ianlee92 <ethian9227@gmail.com>
2021-08-02 19:57:59 +0900

    정규표현식 문자열 변환

--- front/components/Chat/index.tsx
+++ front/components/Chat/index.tsx
@@ -1,14 +1,45 @@
 import { ChatWrapper } from '@components/Chat/styles';
 import { IDM } from '@typings/db';
-import React, { VFC } from 'react';
+import React, { VFC, memo, useMemo } from 'react';
 import gravatar from 'gravatar';
 import dayjs from 'dayjs';
+import regexifyString from 'regexify-string';
+import { Link, useParams } from 'react-router-dom';
 
 interface Props {
   data: IDM;
 }
 const Chat: VFC<Props> = ({ data }) => {
+  const { workspace } = useParams<{ workspace: string; channel: string }>();
   const user = data.Sender; // dm보내는사람 받는사람 Receiver
+
+  // @[이안](1)
+  // \d 숫자 + 는 1개이상 ?는 0개나 1개, * 0개 이상, g 모두찾기
+  // @[이안]12](1)
+  // +는 최대한 많이 이안]12 +?는 최대한 조금 이안
+  // |는 또는 \n는 줄바꿈
+  const result = useMemo(
+    () =>
+      regexifyString({
+        input: data.content,
+        pattern: /@\[(.+?)]\((\d+?)\)|\n/g, // 아이디 줄바꿈
+        decorator(match, index) {
+          const arr: string[] | null = match.match(/@\[(.+?)]\((\d+?)\)/)!; // 줄바꿈빼고 아이디만
+          if (arr) {
+            // 아이디에 걸리는 게 있으면
+            return (
+              // 그 사람한테 dm보내기
+              <Link key={match + index} to={`/workspace/${workspace}/dm/${arr[2]}`}>
+                @{arr[1]}
+              </Link>
+            );
+          }
+          return <br key={index} />; // 줄바꿈
+        },
+      }),
+    [workspace, data.content], // 캐싱을 새로 갱신하는 조건
+  );
+
   return (
     <ChatWrapper>
       <div className="chat-img">
@@ -19,10 +50,10 @@ const Chat: VFC<Props> = ({ data }) => {
           <b>{user.nickname}</b>
           <span>{dayjs(data.createdAt).format('h:mm A')}</span>
         </div>
-        <p>{data.content}</p>
+        <p>{result}</p>
       </div>
     </ChatWrapper>
   );
 };
 
-export default Chat;
+export default memo(Chat);
